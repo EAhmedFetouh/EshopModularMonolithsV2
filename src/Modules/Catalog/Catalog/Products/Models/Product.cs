d57d1df5f08a6fc7d1c361925ab9860d47784a40
EAhmedFetouh <ahmedfetouh2020@yahoo.com>
2025-11-30 20:10:39 +0300

    Intial Modular Monilithic

---
+++ src/Modules/Catalog/Catalog/Products/Models/Product.cs
@@ -0,0 +1,54 @@
+﻿
+using Catalog.Products.Events;
+
+namespace Catalog.Products.Models
+{
+    public class Product : Aggregate<Guid>
+    {
+        public string Name { get; private set; } = default!;
+        public List<string> Category { get; private set; } = new();
+        public string Description { get; private set; }=default!;
+        public string ImageFile { get; private set; } = default!;
+        public decimal Price { get; private set; }
+
+        public static Product Create(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+            var product= new Product
+            {
+                Id = Guid.NewGuid(),
+                Name = name,
+                Category = category,
+                Description = description,
+                ImageFile = imageFile,
+                Price = price
+            };
+
+            product.AddDomainEvent(new ProductCreatedEvent(product));
+            return product;
+
+
+        }
+
+        public void Update(string name, List<string> category, string description, string imageFile, decimal price)
+        {
+            ArgumentException.ThrowIfNullOrEmpty(name);
+            ArgumentOutOfRangeException.ThrowIfNegativeOrZero(price);
+            Name = name;
+            Category = category;
+            Description = description;
+            ImageFile = imageFile;
+            Price = price;
+
+            //if price has changed, raise domain event ProductPriceChanged
+            if(Price != price)
+            {
+                Price = price;
+                AddDomainEvent(new ProductPriceChangedEvent(this));
+            }
+        }
+
+    }
+}
